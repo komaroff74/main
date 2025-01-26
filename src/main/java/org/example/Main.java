34cc505d1db0dcbe204c3309445c4dfd9b981912
Komarov <komaroff74@yandex.ru>
2025-01-26 20:19:53 +0500

    initial commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,16 +1,69 @@
 package org.example;
+
 public class Main {
     public static void main(String[] args) {
 
-        Person[] perArray = new Person[5];
+        Dog dog1 = new Dog("Рекс");
+        Dog dog2 = new Dog("Риц");
 
-        perArray[0] = new Person("Иванов Иван Иванович", "Инженер", "ivan@mailbox.com", "2222222", 10_000, 20);
-        perArray[1] = new Person("Иванов Иван Сергеевич ", "Архитектор", "ivan@mail.com", "3333333", 20_000, 30);
-        perArray[2] = new Person("Иванов Иван Максимович", "Конструктор", "ivan@mail.ru", "4444444", 30_000, 40);
-        perArray[3] = new Person("Иванов Иван Федорович", "Механик", "ivan@mail.com", "5555555", 40_000, 50);
-        perArray[4] = new Person("Иванов Иван Степанович", "Техник", "ivan@maik.com", "6666666", 50_000, 60);
+        Cat cat1 = new Cat("Соня");
+        Cat cat2 = new Cat("Мурка");
+        Cat cat3 = new Cat("Ночька");
 
+        System.out.println("Всего животных: " + Animal.getAnimalCount());
+        System.out.println("Всего собак: " + Dog.getDogCount());
+        System.out.println("Всего котов: " + Cat.getCatCount());
+
+        dog1.run(300);
+        dog1.swim(5);
+
+        dog2.run(600);
+        dog2.swim(15);
+
+        cat1.run(150);
+        cat1.swim(10);
+
+        Bowl bowl = new Bowl(20);
+        Cat[] cats = {cat1, cat2, cat3};
+
+        for (Cat cat : cats) {
+            cat.eat(bowl, 10);
+            System.out.println(cat.getName() + " сыт: " + cat.isFull());
+        }
+        System.out.println("Остаток еды в миске: " + bowl.getFood() + " ед.");
+
+        bowl.addFood(15);
+        System.out.println("Добавили еды. Теперь в миске: " + bowl.getFood() + " ед.");
+
+        for (Cat cat : cats) {
+            if (!cat.isFull()) {
+                cat.eat(bowl, 10);
+                System.out.println(cat.getName() + " сыт: " + cat.isFull());
+            }
+        }
+        System.out.println("Остаток еды в миске: " + bowl.getFood() + " ед.");
+        System.out.println("***********");
+
+
+        System.out.println("Задание №2");
+
+        Shape circle = new Circle(3, "Красный ", " Серый");
+        Shape rectangle = new Rectangle(2, 4, "Белый", "Желтый");
+        Shape triangle = new Triangle(5, 6, 3, "Фиолетовый", "Зеленый");
+
+
+        System.out.println("Круг:");
+        circle.printCharacteristics();
+        System.out.println("-----------------");
+
+        System.out.println("Прямоугольник:");
+        rectangle.printCharacteristics();
+        System.out.println("-----------------");
+
+        System.out.println("Треугольник:");
+        triangle.printCharacteristics();
+        System.out.println("-----------------");
     }
-
 }
+
 
